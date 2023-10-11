65d864a1486074cb632842542e2d1b49ff522e8e
Mark Saroufim <marksaroufim@meta.com>
2023-10-10 18:00:33 -0700

    Update timer_cpp_vs_python.py

--- timer_cpp_vs_python.py
+++ timer_cpp_vs_python.py
@@ -7,15 +7,15 @@ a = torch.rand(10000)
 """
 
 setup_cpp = """
-auto a = torch.rand({10000});
+auto a = torch::rand({10000});
 """
 
 code = """
-a.add(1)
+a.add(1);
 """
 
 t = Timer(setup=setup_py, stmt=code, language=Language.PYTHON)
-print(t.timeit(1000))
+print(t.timeit(100))
 
 t = Timer(setup=setup_cpp, stmt=code, language=Language.CPP)
-print(t.timeit(1000))
+print(t.timeit(100))
